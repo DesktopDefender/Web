97c7de9c519d91f6306da2f4cba9553121cf66e5
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2024-04-09 11:04:08 +0000

    remove download links for unsupported versions

--- src/app/download/page.tsx
+++ src/app/download/page.tsx
@@ -17,37 +17,37 @@ export default function Downloads() {
 
       <div className="mt-10">
         <div className="grid grid-cols-4">
-          <a href="/download.txt" download="DesktopDefender">
-            <div className="flex flex-col items-center p-6 bg-gray-800 rounded-3xl transition-transform duration-500 ease-in-out hover:scale-110">
-              <i className="devicon-apple-original text-6xl" />
-              <span className="mt-4 text-xl">Mac Intel</span>
-            </div>
-          </a>
+          {/* <a href="/download.txt" download="DesktopDefender"> */}
+          <div className="flex flex-col items-center p-6 bg-gray-800 rounded-3xl transition-transform duration-500 ease-in-out hover:opacity-75">
+            <i className="devicon-apple-original text-6xl" />
+            <span className="mt-4 text-xl">Mac Intel</span>
+          </div>
+          {/* </a> */}
 
           <a href="/releases/0.1.0/Desktop Defender_0.1.0_aarch64.dmg">
-            <div className="flex flex-col items-center p-6 bg-gray-800 rounded-3xl transition-transform duration-500 ease-in-out hover:scale-110">
+            <div className="flex flex-col items-center p-6 bg-gray-800 rounded-3xl transition-transform duration-500 ease-in-out hover:scale-105">
               <i className="devicon-apple-original text-6xl" />
               <span className="mt-4 text-xl">Mac ARM</span>
             </div>
           </a>
 
-          <a href="/download.txt" download="DesktopDefender">
-            <div className="flex flex-col items-center p-6 bg-gray-800 rounded-3xl transition-transform duration-500 ease-in-out hover:scale-110">
-              <i className="devicon-windows11-original colored text-6xl" />
-              <span className="mt-4 text-xl">Windows</span>
-            </div>
-          </a>
+          {/* <a href="/download.txt" download="DesktopDefender"> */}
+          <div className="flex flex-col items-center p-6 bg-gray-800 rounded-3xl transition-transform duration-500 ease-in-out hover:opacity-75">
+            <i className="devicon-windows11-original colored text-6xl" />
+            <span className="mt-4 text-xl">Windows</span>
+          </div>
+          {/* </a> */}
 
-          <a href="/download.txt" download="DesktopDefender">
-            <div className="flex flex-col items-center p-5 bg-gray-800 rounded-3xl transition-transform duration-500 ease-in-out hover:scale-110">
-              <img
-                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg"
-                alt="Linux"
-                className="h-16 w-16"
-              />
-              <span className="mt-4 text-xl">Linux</span>
-            </div>
-          </a>
+          {/* <a href="/download.txt" download="DesktopDefender"> */}
+          <div className="flex flex-col items-center p-5 bg-gray-800 rounded-3xl transition-transform duration-500 ease-in-out hover:opacity-75">
+            <img
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg"
+              alt="Linux"
+              className="h-16 w-16"
+            />
+            <span className="mt-4 text-xl">Linux</span>
+          </div>
+          {/* </a> */}
         </div>
       </div>
     </main>
